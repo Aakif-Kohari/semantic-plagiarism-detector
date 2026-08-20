4159fab89bfceaa7d0858a8caaa5511b23973420
Aditya8369 <mahajan2005aditya@gmail.com>
2026-08-20 16:42:37 +0530

    enh: Add CLI downgrade command

--- src/db/migrations/__init__.py
+++ src/db/migrations/__init__.py
@@ -1,10 +1,17 @@
 """Public SQLite migration API."""
 
-from .auth import AUTH_MIGRATIONS, AUTH_SCHEMA_VERSION, migrate_auth_database
+from .auth import (
+    AUTH_DOWN_MIGRATIONS,
+    AUTH_MIGRATIONS,
+    AUTH_SCHEMA_VERSION,
+    migrate_auth_database,
+)
 from .common import (
     check_table_exists,
     column_exists,
     delete_all_if_table_exists,
+    ensure_migration_history_table,
+    get_latest_applied_migration,
     get_migration_status,
     get_user_version,
     index_exists,
@@ -12,15 +19,24 @@ from .common import (
     run_migrations,
     table_exists,
 )
-from .corpus import CORPUS_MIGRATIONS, CORPUS_SCHEMA_VERSION, migrate_corpus_database
+from .corpus import (
+    CORPUS_DOWN_MIGRATIONS,
+    CORPUS_MIGRATIONS,
+    CORPUS_SCHEMA_VERSION,
+    migrate_corpus_database,
+)
 
 __all__ = [
+    "AUTH_DOWN_MIGRATIONS",
     "AUTH_MIGRATIONS",
     "AUTH_SCHEMA_VERSION",
+    "CORPUS_DOWN_MIGRATIONS",
     "CORPUS_MIGRATIONS",
     "CORPUS_SCHEMA_VERSION",
     "column_exists",
     "delete_all_if_table_exists",
+    "ensure_migration_history_table",
+    "get_latest_applied_migration",
     "get_migration_status",
     "get_user_version",
     "index_exists",
